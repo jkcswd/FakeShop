1bac4dfe9c929035cf58697de3f33f70adceeadb
James Spencer <jameskcs1254@gmail.com>
2021-11-25 13:00:53 +0000

    Add basic router structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
+import Nav from "./components/Nav";
 
 const App = () => {
   return (
     <div className="App">
-
+      <header>
+        <Nav/>
+      </header>
+      <main>
+        <BrowserRouter>
+          <Routes>
+            <Route/>
+            <Route/>
+            <Route/>
+          </Routes>
+        </BrowserRouter>
+      </main>
+      <footer></footer>
     </div>
   );
 }
