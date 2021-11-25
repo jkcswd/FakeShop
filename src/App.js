5856e47a0022dab155c9787fe2c15120d8cc8a2b
James Spencer <jameskcs1254@gmail.com>
2021-11-25 18:30:15 +0000

    Transfer Routing from index

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Outlet } from "react-router";
 import './App.css';
 import Nav from "./components/Nav";
-
-
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './components/Home';
+import Shop from './components/Shop';
+import Checkout from './components/Checkout';
 
 const App = () => {
+  // Routes are in this file to allow props to be passed from state to child components while using <Outlet/>.
+  const [products, setProducts] = useState([]);
+
+  useEffect(() =>{
+    fetchProducts();
+  },[]);
+
+  const fetchProducts = async () => {
+    const response = await fetch('https://fakestoreapi.com/products/')
+    const products = response.json()
+    setProducts(products)
+  }
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route index element={<Home/>}/>
+          <Route path="shop" element={<Shop products={products}/>}/>
+          <Route path="checkout" element={<Checkout/>}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+const Layout = () => {
   return (
     <div className="App">
       <header>
--- src/index.js
+++ src/index.js
@@ -2,22 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
-import Home from './components/Home';
-import Shop from './components/Shop';
-import Checkout from './components/Checkout';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App/>}>
-          <Route index element={<Home/>}/>
-          <Route path="shop" element={<Shop/>}/>
-          <Route path="checkout" element={<Checkout/>}/>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
