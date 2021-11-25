953a173a6dfe35fb4541647ca57cebbbbe86a605
James Spencer <jameskcs1254@gmail.com>
2021-11-25 17:48:11 +0000

    Add example text

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,12 @@ import React from "react";
 const Home = () => {
   return (
     <div className="Home">
-
+      <h1>Home</h1>
+      <p>
+        Eu nulla mollit elit magna consequat ea aute ex cillum reprehenderit. Sint quis labore anim in nostrud velit sit labore 
+        ipsum proident ea eu magna. Esse do deserunt ut quis tempor labore et non. Magna sunt excepteur minim exercitation sint
+        deserunt reprehenderit incididunt exercitation nulla esse.
+      </p>
     </div>
   );
 }
--- src/components/Shop.js
+++ src/components/Shop.js
@@ -3,7 +3,7 @@ import React from "react";
 const Shop = () => {
   return (
     <div className="Shop">
-
+      <h1>Shop</h1>
     </div>
   );
 }
