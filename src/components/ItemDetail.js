62a76f7c2d10eba34fc5ce02cf1db2a39cf304f0
James Spencer <jameskcs1254@gmail.com>
2021-11-26 17:01:22 +0000

    Add conditional rendering

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,15 +1,21 @@
 import React from "react";
+import Loading from './Loading'
 
 const ItemDetail = (props) => {
-  // TODO on refresh of page add loading conditions to stop undefined error
   const id = window.location.href.split('/shop/')[1] // Quick fix as match prop not working. TODO: Fix using match.
   const product = props.products[id -1] 
+  
   return (
     <div className="ItemDetail">
-      <h1>{product.title}</h1>
-      <img src={product.image} alt={product.title}/>
-      <p>{product.description}</p>
-      <button data-id={id -1} onClick={props.addToBasket}>Add to basket</button>
+      {props.loading && <Loading/>}
+      {!props.loading && 
+        <div>
+          <h1>{product.title}</h1>
+          <img src={product.image} alt={product.title}/>
+          <p>{product.description}</p>
+          <button data-id={id -1} onClick={props.addToBasket}>Add to basket</button>
+        </div>
+      }
     </div>
   );
 }
