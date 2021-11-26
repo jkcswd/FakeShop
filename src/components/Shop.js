5316ec18ed74f235f457a11c7078cc47b2b1cccd
James Spencer <jameskcs1254@gmail.com>
2021-11-26 12:56:37 +0000

    Import css

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Loading from "./Loading";
 import ShopDisplay from "./ShopDisplay";
+import '../styles/Shop.css'
 
 const Shop = (props) => {
   // Conditional rendering used for loading screen while awaiting API data.
