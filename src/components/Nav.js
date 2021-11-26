a5d36f54330e939c9423da15a41152fa647e8cfc
James Spencer <jameskcs1254@gmail.com>
2021-11-26 16:54:10 +0000

    Pass basketSize prop

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Nav = () => {
+const Nav = (props) => {
   return (
     <div className="Nav">
       <Link to="/">Home</Link>
       <Link to="/shop">Shop</Link>
-      <Link to="/checkout">Checkout</Link>
+      <Link to="/checkout">Checkout({props.basketSize})</Link>
     </div>
   );
 }
