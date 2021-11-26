b90c5f14f27ccf34c128e51aecb1858ab9f02038
James Spencer <jameskcs1254@gmail.com>
2021-11-26 12:57:38 +0000

    Add rendering of API data

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,9 +1,12 @@
 import React from "react";
+import '../styles/Item.css'
 
-const Item = () => {
+const Item = (props) => {
   return (
     <div className="Item">
-
+      <h3>{props.product.title}</h3>
+      <h4>£{props.product.price}</h4>
+      <img src={props.product.image} alt={props.product.title}></img>
     </div>
   );
 }
