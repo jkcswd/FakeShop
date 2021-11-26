951fccf72563bbe9252665f046991fc2d59dfb39
James Spencer <jameskcs1254@gmail.com>
2021-11-26 12:32:15 +0000

    Add conditonal rendering

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,9 +1,14 @@
 import React from "react";
+import Loading from "./Loading";
+import ShopDisplay from "./ShopDisplay";
 
-const Shop = () => {
+const Shop = (props) => {
+  // Conditional rendering used for loading screen while awaiting API data.
   return (
     <div className="Shop">
       <h1>Shop</h1>
+      {props.loading && <Loading/>} 
+      {!props.loading && <ShopDisplay products={props.products}/>}
     </div>
   );
 }
