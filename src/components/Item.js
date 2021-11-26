7ede589761edbfb1e333f9f699f39b8b857035b6
James Spencer <jameskcs1254@gmail.com>
2021-11-26 13:57:32 +0000

    Wrap item component with link to dynamic route

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,20 @@
 import React from "react";
 import '../styles/Item.css'
+import { Link } from "react-router-dom";
 
 const Item = (props) => {
   return (
-    <div className="Item">
-      <h3>{props.product.title}</h3>
-      <h4>£{props.product.price}</h4>
-      <img src={props.product.image} alt={props.product.title}></img>
-    </div>
+      <div className="Item">
+        <Link to={`/shop/${props.product.id}`}>
+          <div>
+            <h3>{props.product.title}</h3>
+            <h4>£{props.product.price}</h4>
+            <img src={props.product.image} alt={props.product.title}></img>
+          </div>
+        </Link>
+      </div>
+   
+    
   );
 }
 
