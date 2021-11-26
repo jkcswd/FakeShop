64ea0382ed2c33a1f0511c93bc219fe5942bbe6e
James Spencer <jameskcs1254@gmail.com>
2021-11-26 16:53:05 +0000

    Create addToBasket handler. Add basketsize prop

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import ItemDetail from "./components/ItemDetail";
 
 const App = () => {
   const [products, setProducts] = useState([]);
-  const [basket, setBaskset] = useState([])
+  const [basket, setBasket] = useState([])
   const [loading, setLoading] = useState(true)
 
   useEffect(() =>{
@@ -23,26 +23,34 @@ const App = () => {
     setProducts(products)
     setLoading(false)
   }
+
+  const addToBasket = (e) => {
+    setBasket([...basket, products[e.target.dataset.id]])
+  }
+
   return (
     // Routes are in this file to allow props to be passed from state to child components while using <Outlet/>.
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout/>}>
+        <Route path="/" element={<Layout basketSize={basket.length}/>}>
           <Route index element={<Home/>}/>
           <Route path="shop" element={<Shop products={products} loading={loading}/>}/>
-          <Route path="shop/:id" element={<ItemDetail products={products}/>}/>
-          <Route path="checkout" element={<Checkout/>}/>
+          <Route 
+            path="shop/:id" 
+            element={<ItemDetail products={products} loading={loading} addToBasket={addToBasket}/>}
+          />
+          <Route path="checkout" element={<Checkout basket={basket}/>}/>
         </Route>
       </Routes>
     </BrowserRouter>
   )
 }
 
-const Layout = () => {
+const Layout = (props) => {
   return (
     <div className="App">
       <header>
-        <Nav/>
+        <Nav basketSize={props.basketSize}/>
       </header>
       <main>
         <Outlet/>
