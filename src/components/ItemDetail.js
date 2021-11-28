d0cced908013385339e1ca905341432067ce8dee
James Spencer <jameskcs1254@gmail.com>
2021-11-28 16:53:54 +0000

    Add form for submitting amount of product

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import Loading from './Loading'
 
 const ItemDetail = (props) => {
+  const [amountItems, setAmountItems] = useState(0)
   const id = window.location.href.split('/shop/')[1] // Quick fix as match prop not working. TODO: Fix using match.
   const product = props.products[id -1] 
+
+  const handleChange = (e) => {
+    setAmountItems(e.target.value)
+  }
   
   return (
     <div className="ItemDetail">
@@ -13,7 +18,13 @@ const ItemDetail = (props) => {
           <h1>{product.title}</h1>
           <img src={product.image} alt={product.title}/>
           <p>{product.description}</p>
-          <button data-id={id -1} onClick={props.addToBasket}>Add to basket</button>
+          <form className="add-to-basket" onSubmit={props.addToBasket} data-id={id - 1}>
+            <label>
+              Amount
+              <input type="number" value={amountItems} name="Amount" onChange={handleChange}/>
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
         </div>
       }
     </div>
