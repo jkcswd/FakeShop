bbbf62915b6fc9ee3abe56ccd85580a26ef70ff5
James Spencer <jameskcs1254@gmail.com>
2021-11-30 14:06:36 +0000

    Add JSX markup to display data

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,9 +1,25 @@
 import React from "react";
+import uniqid from 'uniqid'
 
-const Checkout = () => {
+const Checkout = (props) => {
+  // TODO add function to add up quantities of same products
   return (
     <div className="Checkout">
       <h1>Checkout</h1>
+      {props.basket.map((item, index) => {
+        return (
+          <div className="checkout-item" key={uniqid()} > 
+            <div>{item.item.title}</div>
+            <div data-id={index}>
+              <button onClick={props.reduceBasketAmount}>-</button>
+              quantity:{item.amount}
+              <button onClick={props.increaseBasketAmount}>+</button>
+            </div>
+            <div>£{item.item.price * item.amount}</div>
+          </div>
+        )
+      })}
+      <div>Total:£</div>
     </div>
   );
 }
