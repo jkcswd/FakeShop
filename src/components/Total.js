249954245672a250d71fd41b70e884707a85845e
James Spencer <jameskcs1254@gmail.com>
2021-12-05 12:50:29 +0000

    Update JSX for inside table

--- src/components/Total.js
+++ src/components/Total.js
@@ -4,7 +4,7 @@ import '../styles/Total.css'
 const Total = (props) => {
   return (
     <div className="Total">
-      Total:£{
+      £{
         props.basket.reduce((sum,item) => {
           return sum += item.item.price * item.amount
         },0)
